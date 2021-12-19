100ce4c3f22bec8a8343ee91bcb82a1cf62ecdc3
stalliardo187 <darrenstallard27@hotmail.com>
2021-12-19 16:16:06 +0000

    Added files for editing account info

---
+++ b_h_d/lib/screens/account/editEmail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditEmail extends StatefulWidget {
+  EditEmail({Key? key}) : super(key: key);
+
+  @override
+  _EditEmailState createState() => _EditEmailState();
+}
+
+class _EditEmailState extends State<EditEmail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ b_h_d/lib/screens/account/editFullName.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditFullName extends StatefulWidget {
+  EditFullName({Key? key}) : super(key: key);
+
+  @override
+  _EditFullNameState createState() => _EditFullNameState();
+}
+
+class _EditFullNameState extends State<EditFullName> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
