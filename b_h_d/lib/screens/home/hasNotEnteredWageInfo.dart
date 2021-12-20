f959a72c07d4432806b90ef3073654c05ddbeb2d
stalliardo187 <darrenstallard27@hotmail.com>
2021-12-20 10:27:30 +0000

    Formatting for the wage info inputs

--- b_h_d/lib/screens/home/hasNotEnteredWageInfo.dart
+++ b_h_d/lib/screens/home/hasNotEnteredWageInfo.dart
@@ -17,8 +17,13 @@ class HasNotEnteredWageInfo extends StatefulWidget {
 
 class _HasNotEnteredWageInfoState extends State<HasNotEnteredWageInfo> {
   final String _emptyFieldError = "Fields cannot be blank";
-  final paymentFrequencyOptions = ["Weekly", "Fortnightly", "Mothly"];
   String dropDownValue = "Weekly";
+  String? dayRate;
+  String? hoursInWorkDay;
+  String? retentionAmount;
+
+  final paymentFrequencyOptions = ["Weekly", "Fortnightly", "Mothly"];
+  final _formKey = GlobalKey<FormState>();
 
   onItemSelected(String value) {
     setState(() {
@@ -46,81 +51,132 @@ class _HasNotEnteredWageInfoState extends State<HasNotEnteredWageInfo> {
         SizedBox(
           height: 40,
         ),
-        TextFormField(
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return _emptyFieldError;
-            }
-            return null;
-          },
-          decoration: InputDecoration(labelText: "Day rate"),
-          keyboardType: TextInputType.number,
-          inputFormatters: [FilteringTextInputFormatter.allow(StringFormatting.onlyNumbersAndOneDecimal())],
-          style: MyFormStyles.textFormStyle(),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-          // margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            border: Border.all(
-              width: 0.4,
-              color: Colors.blueGrey,
-            ),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          padding: EdgeInsets.fromLTRB(16, 0, 10, 0),
-          child: MyDropDownButton(
-            items: paymentFrequencyOptions,
-            selected: dropDownValue,
-            valueReturned: onItemSelected,
-          ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        TextFormField(
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return _emptyFieldError;
-            }
-            return null;
-          },
-          decoration: InputDecoration(labelText: "Paid hours in work day"),
-          keyboardType: TextInputType.number,
-          inputFormatters: [FilteringTextInputFormatter.allow(StringFormatting.onlyNumbersAndOneDecimal())],
-          style: MyFormStyles.textFormStyle(),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        TextFormField(
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return _emptyFieldError;
-            }
-            return null;
-          },
-          decoration: InputDecoration(labelText: "Retention amount %"),
-          keyboardType: TextInputType.number,
-          inputFormatters: [FilteringTextInputFormatter.allow(StringFormatting.onlyNumbersAndOneDecimal())],
-          style: MyFormStyles.textFormStyle(),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AccountScreen(),
-              ),
-            );
-          },
-          child: Text("Go to account"),
-        ),
+        Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return _emptyFieldError;
+                    }
+                    if (int.parse(value) < 1) {
+                      return "Eh? £0 a day?";
+                    }
+
+                    if (StringFormatting.startsWithAZero(value)) {
+                      return "Values can't start with a 0";
+                    }
+
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      dayRate = value;
+                    });
+                  },
+                  decoration: InputDecoration(labelText: "Day rate"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.allow(StringFormatting.onlyNumbersAndOneDecimal())],
+                  style: MyFormStyles.textFormStyle(),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  // margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 0.4,
+                      color: Colors.blueGrey,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: EdgeInsets.fromLTRB(16, 0, 10, 0),
+                  child: MyDropDownButton(
+                    items: paymentFrequencyOptions,
+                    selected: dropDownValue,
+                    valueReturned: onItemSelected,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return _emptyFieldError;
+                    }
+                    if (int.parse(value) < 1) {
+                      return "0 hours a day is not valid";
+                    }
+                    if (int.parse(value) > 24) {
+                      return "There's not that many hours in a day :)";
+                    }
+                    if (StringFormatting.startsWithAZero(value)) {
+                      return "Values can't start with a 0";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      hoursInWorkDay = value;
+                    });
+                  },
+                  decoration: InputDecoration(labelText: "Paid hours in work day"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.allow(StringFormatting.onlyNumbersAndOneDecimal())],
+                  style: MyFormStyles.textFormStyle(),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return _emptyFieldError;
+                    }
+                    if (StringFormatting.startsWithAZero(value) && value.length > 1) {
+                      return "Values can't start with a 0";
+                    }
+                    if (int.parse(value) > 100) {
+                      return "Retention can't be greater than 100";
+                    }
+
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      retentionAmount = value;
+                    });
+                  },
+                  decoration: InputDecoration(labelText: "Retention amount %"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.allow(StringFormatting.onlyNumbersAndOneDecimal())],
+                  style: MyFormStyles.textFormStyle(),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print("dr = $dayRate");
+                      print("paymentF = $dropDownValue");
+                      print("hiwd = $hoursInWorkDay");
+                      print("ra = $retentionAmount");
+
+                      // TODO parse to doubles before sending to the db
+
+                      // Now get the values for each and call some update method in the database
+                      // Also need a stream for listening to changes on the user model
+                      // And what about checking for dirty values when the user presses back???
+                    }
+                  },
+                  child: Text("Save Information"),
+                ),
+              ],
+            ))
       ],
     );
   }
--- b_h_d/lib/utils/stringFormatting.dart
+++ b_h_d/lib/utils/stringFormatting.dart
@@ -2,4 +2,8 @@ class StringFormatting {
   static RegExp onlyNumbersAndOneDecimal() {
     return RegExp(r"^\d*\.?\d{0,2}");
   }
+
+  static bool startsWithAZero(String value) {
+    return value[0] == "0";
+  }
 }
