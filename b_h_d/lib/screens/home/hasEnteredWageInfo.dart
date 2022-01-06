7139ffb75c5adbbc107dc0a7675bf5d96f788c5c
stalliardo187 <darrenstallard27@hotmail.com>
2022-01-06 08:48:37 +0000

    Removed unnecessary streamProvider from EmailVerified widget and removed the file.

--- b_h_d/lib/screens/home/hasEnteredWageInfo.dart
+++ b_h_d/lib/screens/home/hasEnteredWageInfo.dart
@@ -1,4 +1,6 @@
+import 'package:b_h_d/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HasEnteredWageInfo extends StatefulWidget {
   HasEnteredWageInfo({Key? key}) : super(key: key);
@@ -10,8 +12,10 @@ class HasEnteredWageInfo extends StatefulWidget {
 class _HasEnteredWageInfoState extends State<HasEnteredWageInfo> {
   @override
   Widget build(BuildContext context) {
+    MyUser _user = Provider.of<MyUser>(context);
+
     return Container(
-      child: Text("Has entered wage info"),
+      child: Text("Your Day Rate is: ${_user.dayRate}"),
     );
   }
 }
--- b_h_d/lib/screens/home/home.dart
+++ b_h_d/lib/screens/home/home.dart
@@ -1,7 +1,11 @@
+import 'package:b_h_d/models/user.dart';
 import 'package:b_h_d/screens/account/account.dart';
 import 'package:b_h_d/screens/home/emailNotVerified.dart';
-import 'package:b_h_d/screens/home/emailVerified.dart';
+import 'package:b_h_d/screens/home/hasEnteredWageInfo.dart';
+import 'package:b_h_d/screens/home/hasNotEnteredWageInfo.dart';
 import 'package:b_h_d/services/authentication.dart';
+import 'package:b_h_d/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,65 +18,77 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Widget _loadScreen(bool hasEnteredWageInfo, String uid) {
+    return hasEnteredWageInfo ? HasEnteredWageInfo() : HasNotEnteredWageInfo(uid: uid);
+  }
+
   @override
   Widget build(BuildContext context) {
     Auth _auth = Provider.of<Auth>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              await Auth().signOut();
-            },
-            icon: Icon(Icons.logout),
-          ),
-        ],
-      ),
-      drawer: _auth.user.hasEnteredWageInfo
-          ? Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  Container(
-                    height: 140,
-                    child: DrawerHeader(
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                      ),
-                      child: Text('Drawer Header'),
-                    ),
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Account',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AccountScreen(),
+    return StreamProvider<MyUser>.value(
+      value: MyDatabase().userData(_auth.user.uid),
+      initialData: MyUser(accountCreated: Timestamp.now()),
+      child: Consumer<MyUser>(
+        builder: (context, _userData, _) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Home"),
+              actions: [
+                IconButton(
+                  onPressed: () async {
+                    await Auth().signOut();
+                  },
+                  icon: Icon(Icons.logout),
+                ),
+              ],
+            ),
+            drawer: _userData.hasEnteredWageInfo
+                ? Drawer(
+                    child: ListView(
+                      padding: EdgeInsets.zero,
+                      children: [
+                        Container(
+                          height: 140,
+                          child: DrawerHeader(
+                            decoration: BoxDecoration(
+                              color: Colors.blueGrey,
+                            ),
+                            child: Text('Drawer Header'),
+                          ),
                         ),
-                      );
-                    },
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Tools',
-                      style: TextStyle(color: Colors.white),
+                        ListTile(
+                          title: Text(
+                            'Account',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onTap: () {
+                            Navigator.pop(context);
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AccountScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text(
+                            'Tools',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
                     ),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-            )
-          : null,
-      body: widget.emailVerified ? EmailVerified() : EmailNotVerified(),
+                  )
+                : null,
+            body: widget.emailVerified ? _loadScreen(_userData.hasEnteredWageInfo, _userData.uid) : EmailNotVerified(),
+          );
+        },
+      ),
     );
   }
 }
